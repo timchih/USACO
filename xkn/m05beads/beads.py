b73db75ba43c31328cc6b087bb1bb07db46621c6
xknxknqq <1553730177@qq.com>
2019-06-30 12:39:46 +0800

    3 red color done

--- xkn/m05beads/beads.py
+++ xkn/m05beads/beads.py
@@ -24,21 +24,72 @@ def cutbeads(cutpoint):
     print("\n")
     return cutedbeads
 
+def firsthcolor(ibeads):
+    for b in ibeads:
+        if b !="w":
+            return b
+        else:
+            continue
+    return "w"
+
+def firsttcolor(ibeads):
+    for i in range(1,len(ibeads)):
+        if ibeads[-i] !="w":
+            return ibeads[-i]
+        else:        
+            continue
+    return "w"
+
+def paintbeads(cbeads):
+    hc=firsthcolor(cbeads)
+    tc=firsttcolor(cbeads)
+    for i in range(len(cbeads)):
+        if cbeads[i]=="w":
+            cbeads[i]=hc
+        elif cbeads[i] !=hc:
+            break
+
+    for i in range(1,len(cbeads)):
+        if cbeads[-i]=="w":
+            cbeads[-i]=tc
+        elif cbeads[-i] !=tc:
+            break
+
+    return cbeads
+
+
 def countbeadscolor(cb):
     cblen=len(cb)
-    headc=0
+    headc=1
     tailc=0
     hcolor=cb[0]
+    tcolor=cb[-1]
+    for i in range(1,cblen):
+        if hcolor == cb[i]:
+            headc+=1
+        else:
+            break
+    for i in range(1,cblen):
+        if tcolor == cb[-i]:
+            tailc+=1
+        else:
+            break
+    if headc == cblen:
+        return cblen
     count=headc+tailc
+    print("hc tc count",headc,tailc,count)
     return count
 
 #bp 0 means in front of first beads
 #cb stands for cutted beads
+#pb stands for pinted beads
 for bp in range(blen):
     cb=cutbeads(bp)
-    num=countbeadscolor(cb)
+    pb=paintbeads(cb)
+    print("painted beads:",pb)
+    num=countbeadscolor(pb)
     bcolors.append(num)
-
+print(bcolors)
 maxbeads=max(bcolors)
 outstr=str(maxbeads)
 
